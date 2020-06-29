c3e92dd4332307c5294ba5e3526b13e01453055d
Dmitry <47507037+DmEye@users.noreply.github.com>
2020-06-29 16:42:29 +0300

    Каркас
    Обновил Каркас

--- code.py
+++ code.py
@@ -1,28 +1,28 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-dots = []
+dots = [] # хранит все точки
+# для отрисовки
+x = []
+y = []
 
 class dot:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-def print_arr(a):
-    for i in a:
-        print(i.x, ", ", i.y)
-        print()
-
 def onMouseEvent(event):
     global dots
+    global x
+    global y
     dots.append(dot(event.xdata, event.ydata))
-    print_arr(dots)
+    for i in dots:
+        x.append(i.x)
+        y.append(i.y)
+    plt.scatter(x, y)
+    plt.show()
 
-
-x = np.arange(0, 5 * np.pi, 0.01)
-y = np.sin(x) * np.cos(3 * x)
 fig = plt.figure()
-plt.plot(x, y)
 
 button_press_event_id = fig.canvas.mpl_connect('button_press_event', onMouseEvent)
 
