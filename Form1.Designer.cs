7cff7124f316f85c913d38581a51e6362d3f0e06
Dmitry <47507037+DmEye@users.noreply.github.com>
2020-06-30 00:17:04 +0300

    AddedLinear approximation
    Done Linear approximation. Have some bugs.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.y_label = new System.Windows.Forms.Label();
+            this.x_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.y_label);
+            this.groupBox1.Controls.Add(this.x_label);
+            this.groupBox1.Location = new System.Drawing.Point(852, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(120, 90);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mouse position:";
+            // 
+            // y_label
+            // 
+            this.y_label.AutoSize = true;
+            this.y_label.Location = new System.Drawing.Point(6, 58);
+            this.y_label.Name = "y_label";
+            this.y_label.Size = new System.Drawing.Size(17, 13);
+            this.y_label.TabIndex = 1;
+            this.y_label.Text = "Y:";
+            // 
+            // x_label
+            // 
+            this.x_label.AutoSize = true;
+            this.x_label.Location = new System.Drawing.Point(6, 27);
+            this.x_label.Name = "x_label";
+            this.x_label.Size = new System.Drawing.Size(17, 13);
+            this.x_label.TabIndex = 0;
+            this.x_label.Text = "X:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "b:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(852, 108);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(120, 90);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Function parameters:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(984, 711);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Linear Approximation";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label y_label;
+        private System.Windows.Forms.Label x_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Approximation_dots
 {
     public partial class MainForm : Form
     {
+        private int width;
+        private int height;
+        private float a;
+        private float b;
+        private int sumP, sumX, sumY, sumX2;
+        private int max = int.MinValue;
+        private int min = int.MaxValue;
+        private int x_max;
+        private int x_min;
+        private Graphics g;
+
+        private List<int> x;
+        private List<int> y;
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            width = ClientSize.Width;
+            height = ClientSize.Height;
+
+            x = new List<int>();
+            y = new List<int>();
+
+            sumP = 0;
+            sumX = 0;
+            sumY = 0;
+            sumX2 = 0;
+            a = 0;
+            b = 0;
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            for (int i = 0; i < x.Count; i++)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), x[i] - 4, y[i] - 4, 8, 8);
+            }
+
+            if (min != int.MaxValue && max != int.MinValue)
+            {
+                g.DrawLine(new Pen(Color.Red, 2), x_min, min, x_max, max);
+            }
+        }
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            x.Add(e.X);
+            y.Add(e.Y);
+            if (x.Count > 1)
+            {
+                Approximating();
+                for (int i = 0; i < width; i++)
+                {
+                    if (i * a + b < height)
+                    {
+                        if (i * a + b > max)
+                        {
+                            max = (int)(i * a + b);
+                            x_max = i;
+                        }
+                        if (i * a + b < min)
+                        {
+                            min = (int)(i * a + b);
+                            x_min = i;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            label1.Text = "a: " + a;
+            label2.Text = "b: " + b;
+            Invalidate();
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            x_label.Text = "X: " + e.X;
+            y_label.Text = "Y: " + e.Y;
+        }
+
+        private void Approximating()
+        {
+            sumP = 0;
+            sumX = 0;
+            sumY = 0;
+            sumX2 = 0;
+            a = 0;
+            b = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                sumP += x[i] * y[i];
+                sumX += x[i];
+                sumY += y[i];
+                sumX2 += x[i] * x[i];
+            }
+            a = (float)(x.Count * sumP - sumX * sumY) / (float)(x.Count * sumX2 - sumX * sumX);
+            b = (float)(sumY - a * sumX) / (float)x.Count;
         }
     }
 }
